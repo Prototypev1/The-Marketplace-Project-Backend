3e7b61d3938e1d995b55f83b1fe8052d715e5d52
Prototype v1 <mradisavljevicmas@gmail.com>
2026-02-11 18:32:06 +0100

    entities and user role enum

---
+++ Marketplace.Domain/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using Marketplace.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Marketplace.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get;private set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get;private set; } = string.Empty;
+        public string PasswordHash { get; private set; } = string.Empty;
+
+        public UserRole Role { get; set; } = UserRole.RegularUser;
+        public DateTime CreatedAt { get; private set; }
+
+        private User() { 
+        }
+        public User(string firstName, string lastName, string email, string passwordHash, UserRole userRole)
+        {
+            if(string.IsNullOrWhiteSpace(firstName)) throw new ArgumentException("First name cannot be empty.", nameof(firstName));
+            if(string.IsNullOrWhiteSpace(lastName)) throw new ArgumentException("Last name cannot be empty.", nameof(lastName));
+
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PasswordHash = passwordHash;
+            Role = userRole;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
---
+++ Marketplace.Domain/Enums/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.Domain.Enums
+{
+    public enum UserRole
+    {
+        Admin = 0,
+        RegularUser = 1,
+        PremiumUser = 2,
+        RegularBuyer = 3,
+        PremiumBuyer = 4,
+    }
+}
